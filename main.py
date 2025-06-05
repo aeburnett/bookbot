5d15bcd06aaf53f297c5a7fcba28ca391cd14ec6
aeburnett <adameburnett@gmail.com>
2025-06-04 21:26:37 -0600

    created report of book

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from stats import get_word_count, get_character_count
+from stats import get_word_count, get_character_count, get_sorted_dict
 
 def main():
     book_path = ("books/frankenstein.txt")
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
-    print(f"{word_count} words found in the document")
     character_count = get_character_count(book_text)
-    print(character_count)
+    sorted_dict = get_sorted_dict(character_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for d in sorted_dict:
+        if(d["char"].isalpha()):
+            print(f"{d["char"]}: {d["num"]}")
 
 def get_book_text(file):
     with open(file) as f:
--- stats.py
+++ stats.py
@@ -12,3 +12,14 @@ def get_character_count(text):
                 character_count[w] = 1
             else: character_count[w] += 1
     return character_count
+
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_dict(dict):
+    sorted_list = []
+    for k in dict:
+        sorted_list.append({"char": k, "num": dict[k]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
