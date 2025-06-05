0d345943310ee0342a2df12ea967ca8f13e48720
aeburnett <adameburnett@gmail.com>
2025-06-04 21:38:41 -0600

    finished project, now can accept any book and do a report on it.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from stats import get_word_count, get_character_count, get_sorted_dict
+import sys
 
 def main():
-    book_path = ("books/frankenstein.txt")
+    if(len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = (sys.argv[1])
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
     character_count = get_character_count(book_text)
